987c5013e238b646a87f1d3539bd7136ef27cfd2
AC12arpitChauhan <arpitchauhanac07@gmail.com>
2024-11-09 23:15:37 +0530

    updated Landing Page

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -7,7 +7,8 @@ function HeroSection() {
   return (
     <div className="hero-container">
       <div className="hero-content">
-        <h1>Where your music is everything</h1>
+        <h1>Where your music</h1>
+        <h1 id='below'>is everything.</h1>
         <p>Develop your fanbase, build your business, and create the world around your music.</p>
         <div className="hero-buttons">
           <Link to='/dashboard'><button>Go to Dashboard</button></Link>
